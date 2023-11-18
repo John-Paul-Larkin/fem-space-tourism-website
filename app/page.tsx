2b4977f704864d8c2a63e91224fea3318970f475
John Paul Larkin <johnplarkin@gmail.com>
2023-11-18 14:40:19 +0000

    add tailwind suctom theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-home-mobile tablet:bg-home-tablet desktop:bg-home-desktop"></main>
+    <main
+      className="flex h-[calc(100vh-112px)] flex-col items-center justify-around bg-home-mobile 
+    bg-cover bg-center  tablet:bg-home-tablet desktop:bg-home-desktop"
+    >
+      <div className="mx-[24px] text-center">
+        <h5 className="text-fs-300 tracking-ls-4 uppercase ">
+          So, you want to travel to
+        </h5>
+        <h1 className="text-fs-900 my-[16px] font-bellefair uppercase leading-[125%]">
+          Space
+        </h1>
+
+        <p className="text-fs-200 leading-[25px]">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+
+      <div className=" ">
+        <Link
+          href="/"
+          className="flex h-[9.375rem] w-[9.375rem] items-center justify-center rounded-full bg-white text-black"
+        >
+          <span className="text-fs-500 font-bellefair uppercase tracking-[1.25px]">
+            Explore
+          </span>
+        </Link>
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,12 @@
 import type { Config } from "tailwindcss";
 
+// Custom line-heights
+// 25 1.5625rem
+// 28 1.6255rem
+// 32 25rem
+// 100 6.255rem
+// 150 9.3755rem
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,7 +14,34 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
- 
+    extend: {
+      fontFamily: {
+        barlow_condensed: ["var(--font-barlow-condensed)"],
+        bellefair: ["var(--font-bellefair)"],
+      },
+      letterSpacing: {
+        "ls-1": "1.25px",
+        "ls-2": "2px",
+        "ls-3": "2.362px",
+        "ls-4": "2.7px",
+        "ls-5": "3.375px",
+        "ls-6": "4.725px",
+      },
+    },
+    fontSize: {
+      "fs-100": "0.875rem",
+      "fs-200": "0.9375rem",
+      "fs-300": "1rem",
+      "fs-400": "1.125rem",
+      "fs-500": "1.25rem",
+      "fs-600": "1.75rem",
+      "fs-700": "2rem",
+      "fs-800": "3.5rem",
+      "fs-900": "5rem",
+      "fs-1000": "6.25rem",
+      "fs-1100": "9,375rem",
+    },
+
     screens: {
       tablet: "480px",
       desktop: "769px",
@@ -16,6 +50,8 @@ const config: Config = {
       white: "hsl(0, 0%, 100%)",
       lilac: "hsl(231, 77%, 90%)",
       black: "hsl(230, 25%, 7%)",
+      yellow: "hsl(59, 100%, 75%)",
+      brown: "hsl(48, 77%, 42%)",
     },
     backgroundImage: {
       "home-mobile": "url('/home/background-home-mobile.jpg')",
