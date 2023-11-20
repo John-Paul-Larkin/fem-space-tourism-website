c775871c5b824cc7fc4ecbe77b66b49119317dca
John Paul Larkin <johnplarkin@gmail.com>
2023-11-20 00:12:43 +0000

    refactor clean tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,124 +2,34 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className="
-  
-     bg-home-mobile bg-cover 
-    bg-bottom
-    tablet:bg-home-tablet desktop:bg-home-desktop
-    "
- 
-    >
+    <main className="bg-home-mobile bg-cover bg-bottom tablet:bg-home-tablet desktop:bg-home-desktop">
       <div
-        className="   
-       desktop:max-w-container-width
-       desktop:mx-auto
-
-
-       flex
-       flex-col items-center justify-between 
-       desktop:flex-row
-       desktop:items-end
-       
-       desktop:justify-around
-       h-screen
-      w-screen 
-   desktop:pb-[8.187rem]
-    "
+        className="flex h-screen w-screen flex-col items-center justify-between desktop:mx-auto
+       desktop:max-w-container-width desktop:flex-row desktop:items-end desktop:justify-around desktop:pb-[8.187rem]"
       >
-        <div
-          className=" mx-6 max-w-[20.44rem] text-center  tablet:max-w-[27.75rem]
-  desktop:m-0
-
-  desktop:w-[50%]
-  desktop:text-left
-  mt-[7rem] tablet:mt-[12.625rem]
-
-
-      "
-        >
-          <h5
-            className="text-fs-300 tracking-ls-4 tablet:text-fs-500 tablet:tracking-ls-5 desktop:text-fs-600 desktop:tracking-ls-6 font-barlow_condensed
-        uppercase text-lilac
-        
-        
-        "
-          >
+        <div className="mx-6 mt-[7rem] max-w-[20.44rem] text-center tablet:mt-[12.625rem] tablet:max-w-[27.75rem] desktop:m-0 desktop:w-[50%] desktop:text-left">
+          <h5 className="font-barlow_condensed text-fs-300 uppercase tracking-ls-4 text-lilac tablet:text-fs-500 tablet:tracking-ls-5 desktop:text-fs-600 desktop:tracking-ls-6">
             So, you want to travel to
           </h5>
-          <h1
-            className="text-fs-900 leading-lh-4 tablet:text-fs-1100 tablet:leading-lh-5 desktop:text-fs-1100 my-4 font-bellefair uppercase
-        
-          tablet:my-6 desktop:my-6 desktop:leading-[1.1]
-        
-        "
-          >
+          <h1 className="my-4 font-bellefair text-fs-900 uppercase leading-lh-4 tablet:my-6 tablet:text-fs-1100 tablet:leading-lh-5 desktop:my-6 desktop:text-fs-1100 desktop:leading-[1.1]">
             Space
           </h1>
 
-          <p
-            className="text-fs-200 leading-lh-1 tablet:text-fs-300 tablet:leading-lh-2 desktop:text-fs-400
-        desktop:leading-lh-3 text-lilac
-        "
-          >
+          <p className="text-fs-200 leading-lh-1 text-lilac tablet:text-fs-300 tablet:leading-lh-2 desktop:text-fs-400 desktop:leading-lh-3">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
           </p>
         </div>
-        <div
-          className="   
-  
-     
-  
- 
-    
-
-      "
-        >
+        <div>
           <Link
             href="/"
             className="before:content[''] relative z-30 mb-12 flex h-[9.375rem] w-[9.375rem] items-center justify-center rounded-full
-          bg-white text-black 
-          after:absolute
-          after:z-20
-          after:h-[9.375rem] after:w-[9.375rem]
-          after:rounded-full 
-          after:bg-white
-          after:opacity-10
-          after:transition-all
-          after:duration-500
-          after:ease-in-out
-          after:hover:scale-150         
-          after:focus:scale-150 
-
-          tablet:mb-[5.625rem]         
-          tablet:h-[15.125rem] 
-
-          tablet:w-[15.125rem]
-          tablet:after:h-[15.125rem] tablet:after:w-[15.125rem] desktop:m-0
-          
-         
-
-          desktop:h-[17.125rem]
-
-          desktop:w-[17.125rem]
-
-
-
-          "
+          bg-white text-black after:absolute after:z-20 after:h-[9.375rem] after:w-[9.375rem] after:rounded-full after:bg-white after:opacity-10 after:transition-all after:duration-500
+          after:ease-in-out after:hover:scale-150 after:focus:scale-150 tablet:mb-[5.625rem] tablet:h-[15.125rem] tablet:w-[15.125rem] tablet:after:h-[15.125rem] tablet:after:w-[15.125rem] desktop:m-0 desktop:h-[17.125rem] desktop:w-[17.125rem]"
           >
-            <span
-              className="text-fs-500 tracking-ls-1 tablet:text-fs-700 tablet:tracking-ls-2 desktop:text-fs-700 desktop:tracking-ls-2
-          font-bellefair
-          uppercase
-
-
-
-"
-            >
+            <span className="font-bellefair text-fs-500 uppercase tracking-ls-1 tablet:text-fs-700 tablet:tracking-ls-2 desktop:text-fs-700 desktop:tracking-ls-2">
               Explore
             </span>
           </Link>
@@ -128,6 +38,3 @@ export default function Home() {
     </main>
   );
 }
-
-// desktop:mb-[8.187rem]
-//  desktop:mb-[8.187rem]
---
+++ components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import logo from "@/public/shared/logo.svg";
+import hamburger from "@/public/shared/icon-hamburger.svg";
+import close from "@/public/shared/icon-close.svg";
+import Link from "next/link";
+import { useState } from "react";
+import React from "react";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="absolute w-screen">
+      <div className="z-40 mx-auto flex h-[5.5rem] w-full max-w-container-width items-center tablet:h-24 desktop:mt-10">
+        <div className="relative ml-6 h-10 w-10 tablet:ml-10 tablet:h-12 tablet:w-12  tablet:min-w-[3rem] desktop:mx-14">
+          <Image priority src={logo} fill={true} alt="logo" />
+        </div>
+        <div className="grow">
+          <div className="hidden lrg-desktop:relative lrg-desktop:z-50 lrg-desktop:block lrg-desktop:h-[1px] lrg-desktop:w-[calc(100%+2rem)] lrg-desktop:bg-white lrg-desktop:opacity-25"></div>
+        </div>
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="relative z-40 mr-6 text-white tablet:hidden"
+        >
+          {!isMenuOpen && <Image src={hamburger} alt="hamburger menu" />}
+          {isMenuOpen && <Image src={close} alt="close menu" />}
+        </button>
+
+        <nav
+          className={
+            isMenuOpen
+              ? "absolute right-0 top-0  block h-screen w-[15.875rem] bg-white bg-opacity-10 text-white backdrop-blur-xl"
+              : "hidden bg-black-rgba tablet:block tablet:h-24 tablet:w-[28.125rem] desktop:w-[51.875rem] desktop:bg-opacity-90 desktop:text-white desktop:backdrop-blur-xl"
+          }
+        >
+          <ul className="ml-7 font-barlow_condensed text-fs-300 uppercase tracking-ls-4 tablet:flex tablet:h-full tablet:items-center tablet:justify-evenly tablet:text-fs-100 tablet:tracking-ls-3 desktop:mx-0 desktop:justify-evenly desktop:text-fs-300 desktop:tracking-ls-4 lrg-desktop:desktop:ml-28 lrg-desktop:desktop:mr-40 lrg-desktop:justify-between">
+            <NavMenuItem menuItem="home" index="00" />
+            <NavMenuItem menuItem="destination" index="01" />
+            <NavMenuItem menuItem="crew" index="02" />
+            <NavMenuItem menuItem="technology" index="03" />
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+const NavMenuItem = ({
+  menuItem,
+  index,
+}: {
+  index: string;
+  menuItem: string;
+}) => {
+  return (
+    <li className="after:tablet:content[''] relative mt-8 cursor-pointer tablet:m-0 tablet:flex tablet:h-full tablet:items-center after:tablet:absolute after:tablet:h-full after:tablet:w-full after:tablet:hover:border-b-[3px] ">
+      <Link
+        href={`/${menuItem}`}
+        className="z-50 tablet:flex tablet:h-full tablet:items-center"
+      >
+        <span className="mr-3 font-bold tablet:hidden desktop:inline">
+          {index}
+        </span>
+        {menuItem}
+      </Link>
+    </li>
+  );
+};
